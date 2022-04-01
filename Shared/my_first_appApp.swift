fc4c0beba3d2dd692b0a9785799e6ad389305594
pachizamora <maripaz.zamora@gmail.com>
2022-04-01 17:17:04 -0600

    Initial Commit

---
+++ Shared/my_first_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  my_first_appApp.swift
+//  Shared
+//
+//  Created by Maripaz Zamora on 4/1/22.
+//
+
+import SwiftUI
+
+@main
+struct my_first_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
